8861e8eafbbc98adb725f3b14107432099e490ad
Aurélie <aurelie.demure.ad@gmail.com>
2023-06-28 21:48:04 +0200

    added login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,23 +1,89 @@
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import Button from "@mui/material/Button";
+// import TextField from "@mui/material/TextField";
 import styles from "./Login.module.css";
+import { useUserContext } from "../../contexts/userContext";
+import APIService from "../../services/APIService";
 
 export default function Login() {
+  const { login } = useUserContext();
+  const [userInfos, setUserInfos] = useState({ lastname: "", password: "" });
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await APIService.post(`/login`, userInfos);
+      if (res) {
+        login(res.data);
+        if (res.data.roles === "admin") {
+          navigate("/admin");
+        } else {
+          navigate("/home");
+        }
+      } else {
+        throw new Error();
+      }
+    } catch {
+      if (error.response?.status === 401) {
+        setError("Les informations d'identification sont incorrectes.");
+      } else {
+        setError("Une erreur s'est produite. Veuillez réessayer plus tard.");
+      }
+    }
+  };
+  const handleChange = (e) => {
+    setUserInfos({
+      ...userInfos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className={styles.login_page}>
       <div className={styles.login_box}>
-        <TextField
+        <form action="login" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="lastname"
+              name="lastname"
+              id="lastname"
+              required="required"
+              placeholder="lastname"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required="required"
+              placeholder="........"
+              onChange={handleChange}
+            />
+            <button type="submit">Connexion</button>
+          </div>
+        </form>
+
+        {/* <TextField
           id="outlined-basic"
           label="Nom d'utilisateur"
           variant="outlined"
+          value={userInfos.lastname}
         />
         <TextField
           id="outlined-basic"
           label="Mot de passe"
           variant="outlined"
-        />
-        <Link to="/home">
+          value={userInfos.password}
+        /> */}
+
+        {/* <Link to="/home">
           <Button type="button" variant="contained">
             User
           </Button>
@@ -26,7 +92,7 @@ export default function Login() {
           <Button type="button" variant="contained">
             Admin
           </Button>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
