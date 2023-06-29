446e28ad4a3473648f7cac2a760155315ec06a98
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2023-06-30 01:07:10 +0200

    finished home

--- frontend/src/components/AdminComponents/AdminHome/AdminHome.jsx
+++ frontend/src/components/AdminComponents/AdminHome/AdminHome.jsx
@@ -1,11 +1,34 @@
-// import { Link } from "react-router-dom";
-// import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 import styles from "./AdminHome.module.css";
 
 export default function AdminHome() {
   return (
     <div className={styles.admin_home_container}>
-      <h1>WELCOME ON ADMIN PAGE</h1>
+      <Link to="telephone-list">
+        <div className={styles.admin_box}>
+          <div className={styles.overlay}>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "rgb(158, 68, 68)" }}
+            >
+              Voir les Stocks
+            </Button>
+          </div>
+        </div>
+      </Link>
+      <Link to="telephone-list">
+        <div className={styles.admin_box2}>
+          <div className={styles.overlay}>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "rgb(158, 68, 68)" }}
+            >
+              Ajouter un Utilisateur
+            </Button>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
